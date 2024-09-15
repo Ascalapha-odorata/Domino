bc92af9497f371157af0b1572bcf6172c67d7d3f
Ascalapha-odorata <135762969+Ascalapha-odorata@users.noreply.github.com>
2024-09-14 21:26:57 -0300

    Add files via upload

---
+++ Dom_ALGH_Main.cpp
@@ -0,0 +1,16 @@
+/*
+DOM_ALGH_Main.cpp
+DOM-ALGH - Projeto Domino
+2023
+*/
+
+#include "Dom_ALGH_Model.cpp"
+#include "Dom_ALGH_Controller.cpp"
+
+
+int main (){
+	IniciarPedras();
+	executarMenu(); 	//no controller
+
+}
+
